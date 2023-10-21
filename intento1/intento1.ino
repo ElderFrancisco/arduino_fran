e5ddf33c103bc5f6ade75f4c375cf454a99adfa9
Francisco <franciscoelder0@gmail.com>
2023-10-21 18:44:39 -0300

    2

---
+++ intento1/intento1.ino
@@ -0,0 +1,59 @@
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+LiquidCrystal_I2C lcd(0x3f,16,2); // si no te sale con esta direccion  puedes usar (0x3f,16,2) || (0x27,16,2)  ||(0x20,16,2) 
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup() {
+  
+  Serial.begin(9600);
+  SPI.begin();
+  mfrc522.PCD_Init();
+  Serial.println("Lector RFID listo.");
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Lector RFID");
+  lcd.setCursor(0, 1);
+  lcd.print("Listo");
+}
+
+void loop() {
+  if (mfrc522.PICC_IsNewCardPresent()) {
+     Serial.println("tarjeta nueva");
+    
+    if (mfrc522.PICC_ReadCardSerial()) {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Tarjeta UID:");
+      
+          String uidString = ""; // Variable para almacenar el UID como una cadena
+
+      
+      for (byte i = 0; i < mfrc522.uid.size; i++) {
+        uidString += (mfrc522.uid.uidByte[i] < 0x10 ? " 0" : "");
+        uidString += String(mfrc522.uid.uidByte[i], HEX);
+      }
+
+      lcd.setCursor(0, 1);
+      lcd.print(uidString);
+      
+     Serial.println(uidString);
+
+      mfrc522.PICC_HaltA();
+      delay(3000); // Muestra el UID durante 10 segundos antes de borrar la pantalla
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Lector RFID");
+      lcd.setCursor(0, 1);
+      lcd.print("Listo");
+    }
+  }
+}
+
